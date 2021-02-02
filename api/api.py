67cabbeb79a86333bf2cf4790bd1ecc93ae50304
marks214 <aimee.oz.x@gmail.com>
2021-02-01 17:15:23 -0800

    search is now working - if the user looks up a food and it's in the database, it will be returned, if it's not, the external api will be queried and the food will be added to the database

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, json
 import requests
 import os
 import json
@@ -11,87 +11,117 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///foods.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
 base_url = 'https://api.edamam.com/api/food-database/v2/parser'
 app_key = os.getenv('EDAMAM_API_KEY')
 app_ID = os.getenv('EDAMAM_API_ID')
 
-from models import Food, Meal, User
+from models import Food  # , Meal, User
 
 def construct_food(json_data):
-  for i in range(len(json_data) - 1):
-    # check for unique external id
-    external_id = json_data[i]['food']['foodId']
-    exists = Food.query.filter_by(external_id=external_id).first()
+    for i in range(len(json_data) - 1):
+        # check for unique external id
+        external_id = json_data[i]['food']['foodId']
+        exists = Food.query.filter_by(external_id=external_id).first()
 
-    if exists is None:
-      name = json_data[i]['food']['label']
+        if exists is None:
+            name = json_data[i]['food']['label']
 
-      energy = json_data[i]['food']['nutrients']['ENERC_KCAL'] if   'ENERC_KCAL' in json_data[i]['food']['nutrients'].keys()  else 0
+            energy = json_data[i]['food']['nutrients']['ENERC_KCAL'] if 'ENERC_KCAL' in json_data[i]['food']['nutrients'].keys(
+            ) else 0
 
-      protein = json_data[i]['food']['nutrients']['PROCNT'] if  'PROCNT' in json_data[i]['food']['nutrients'].keys() else 0
+            protein = json_data[i]['food']['nutrients']['PROCNT'] if 'PROCNT' in json_data[i]['food']['nutrients'].keys(
+            ) else 0
 
-      carbohydrate = json_data[i]['food']['nutrients']['CHOCDF']  if 'CHOCDF' in json_data[i]['food']['nutrients'].keys() else   0
+            carbohydrate = json_data[i]['food']['nutrients']['CHOCDF'] if 'CHOCDF' in json_data[i]['food']['nutrients'].keys(
+            ) else 0
 
-      fat = json_data[i]['food']['nutrients']['FAT'] if 'FAT' in  json_data[i]['food']['nutrients'].keys() else 0
+            fat = json_data[i]['food']['nutrients']['FAT'] if 'FAT' in json_data[i]['food']['nutrients'].keys(
+            ) else 0
 
-      fiber = json_data[i]['food']['nutrients']['FIBTG'] if   'FIBTG' in json_data[i]['food']['nutrients'].keys() else 0
+            fiber = json_data[i]['food']['nutrients']['FIBTG'] if 'FIBTG' in json_data[i]['food']['nutrients'].keys(
+            ) else 0
 
-      external_id = json_data[i]['food']['foodId']
+            external_id = json_data[i]['food']['foodId']
 
-      food = Food(name=name, energy=energy, protein=protein,  carbohydrate=carbohydrate, fat=fat, fiber=fiber,   external_id=external_id)
-      db.session.add(food)
-      db.session.commit()
-      print(f'{food.name} added to database')
+            food = Food(name=name, energy=energy, protein=protein,
+                        carbohydrate=carbohydrate, fat=fat, fiber=fiber,   external_id=external_id)
+            db.session.add(food)
+            db.session.commit()
+            print(f'{food.name} added to database')
 
-    else:
-      pass
+        else:
+            pass
 
-@app.route('/', defaults={'food' : ''})
-@app.route('/<food>', methods= ['GET', 'POST'])
+
+def food_serializer(food):
+    return {
+        'id': food.id,
+        'name': food.name,
+        'energy': food.energy,
+        'protein': food.protein,
+        'carbohydrate': food.carbohydrate,
+        'fat': food.fat,
+        'fiber': food.fiber,
+        'external_id': food.external_id
+    }
+
+
+@app.route('/', defaults={'food': ''})
+@app.route('/<food>', methods=['GET'])
 def index(food):
-  url = f'{base_url}?ingr={food}&app_id={app_ID}&app_key={app_key}'
-  response = requests.get(url)
     # response = 200, response.content is the json with \n, \b, etc.
     # data = response.json()['hints']
     # json.loads converts the data into a python object
-  if food == '':
-    return {
-      'name' : 'Hello World!'
-      }
-  else:
-    if request.method == 'POST':
-      if request.is_json:
-        data = request.get_json()
-        new_food = Food(
-          name=data['name'],
-          energy=data['energy'],
-          protein=data['protein'],
-          carbohydrate=data['carbohydrate'],
-          fat=data['fat'],
-          fiber=data['fiber'])
-        db.session.add(new_food)
-        db.session.commit()
-        return {"message": f'{new_food.name} has been created successfully.'}
-      else:
-        return {"error": "The request failed."}
-    elif request.method == 'GET':
-      json_data = json.loads(response.content)
-      results = construct_food(json_data['hints'])
-      all_foods = Food.query.all()
-      foods_list = [
-        {
-          'id': food.id,
-          'name': food.name,
-          'energy': food.energy,
-          'protein': food.protein,
-          'carbohydrate': food.carbohydrate,
-          'fat': food.fat,
-          'fiber': food.fiber,
-          'external_id': food.external_id
-        } for food in all_foods]
-      return str(foods_list)
+    search_results = Food.query.filter(Food.name.ilike(f'%{food}%')).all()
+    if len(search_results) == 0:
+        url = f'{base_url}?ingr={food}&app_id={app_ID}&app_key={app_key}'
+        response = requests.get(url)
+        data = response.json()['hints']
+        found_foods = construct_food(data)
+        food_matches = Food.query.filter(Food.name.ilike(f'%{food}%')).all()
+        return jsonify([*map(food_serializer, food_matches)])
+    elif len(search_results) > 0:
+        return jsonify([*map(food_serializer, search_results)])
+    else:
+      pass
+
+    # if request.method == 'POST':
+    #     if request.is_json:
+    #         data = request.get_json()
+    #         new_food = Food(
+    #             name=data['name'],
+    #             energy=data['energy'],
+    #             protein=data['protein'],
+    #             carbohydrate=data['carbohydrate'],
+    #             fat=data['fat'],
+    #             fiber=data['fiber'])
+    #         db.session.add(new_food)
+    #         db.session.commit()
+    #         return {"message": f'{new_food.name} has been created successfully.'}
+    #     else:
+    #         return {"error": "The request failed."}
+    # elif request.method == 'GET':
+    #     url = f'{base_url}?ingr={food}&app_id={app_ID}&app_key={app_key}'
+    #     data = response.json()['hints']
+    #     return jsonify([*map(food_serializer, Food.query.filter_by(Food.name.ilike(f'%{food}%')))])
+        # json_data = json.loads(response.content)
+        # results = construct_food(json_data['hints'])
+        # all_foods = Food.query.all()
+        # foods_list = [
+        #     {
+        #         'id': food.id,
+        #         'name': food.name,
+        #         'energy': food.energy,
+        #         'protein': food.protein,
+        #         'carbohydrate': food.carbohydrate,
+        #         'fat': food.fat,
+        #         'fiber': food.fiber,
+        #         'external_id': food.external_id
+        #     } for food in all_foods]
+        # return str(foods_list)
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
--- api/models.py
+++ api/models.py
@@ -1,62 +1,62 @@
 from api import db
 
 class Food(db.Model):
-  __tablename__ = 'foods'
+    __tablename__ = 'foods'
 
-  id = db.Column(db.Integer, primary_key=True)
-  # nullable = False means it cannot be empty
-  name = db.Column(db.Text, nullable=False)
-  energy = db.Column(db.Float, nullable=True)
-  protein = db.Column(db.Float, nullable=True)
-  carbohydrate = db.Column(db.Float, nullable=True)
-  fat = db.Column(db.Float, nullable=True)
-  fiber = db.Column(db.Float, nullable=True)
-  external_id = db.Column(db.String, nullable=True, unique=True)
-  #db.UniqueConstraint(external_id)
-
-  def __init__(self, name, energy, protein, carbohydrate, fat, fiber, external_id):
-    self.name = name
-    self.energy = energy
-    self.protein = protein
-    self.carbohydrate = carbohydrate
-    self.fat = fat
-    self.fiber = fiber
-    self.external_id = external_id
-
-  def __str__(self):
-    return f'{self.id} {self.name} {self.energy} {self.protein} {self.carbohydrate} {self.fat} {self.fiber} {self.external_id}'
-
-
-class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    # the following are per day (e.g., minimum grams of protein per day)
-    energy_min = db.Column(db.Integer, nullable=True)
-    energy_max = db.Column(db.Integer, nullable=True)
-    protein_min = db.Column(db.Integer, nullable=True)
-    protein_max = db.Column(db.Integer, nullable=True)
-    carb_min = db.Column(db.Integer, nullable=True)
-    carb_max = db.Column(db.Integer, nullable=True)
-    fat_min = db.Column(db.Integer, nullable=True)
-    fat_max = db.Column(db.Integer, nullable=True)
+    # nullable = False means it cannot be empty
+    name = db.Column(db.Text, nullable=False)
+    energy = db.Column(db.Float, nullable=True)
+    protein = db.Column(db.Float, nullable=True)
+    carbohydrate = db.Column(db.Float, nullable=True)
+    fat = db.Column(db.Float, nullable=True)
+    fiber = db.Column(db.Float, nullable=True)
+    external_id = db.Column(db.String, nullable=True, unique=True)
+    # db.UniqueConstraint(external_id)
 
-    def __repr__(self):
-        return '<User %>' % self.username
+    def __init__(self, name, energy, protein, carbohydrate, fat, fiber, external_id):
+        self.name = name
+        self.energy = energy
+        self.protein = protein
+        self.carbohydrate = carbohydrate
+        self.fat = fat
+        self.fiber = fiber
+        self.external_id = external_id
 
-# a meal is an individual food consumed by the user (meals do not have multiple foods)
-class Meal(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    food_id = db.Column(db.Integer, db.ForeignKey('food.id'), nullable=False)
-    name = db.Column(db.String, nullable=False)
-    energy = db.Column(db.Integer, nullable=True)
-    protein = db.Column(db.Integer, nullable=True)
-    carb = db.Column(db.Integer, nullable=True)
-    fat = db.Column(db.Integer, nullable=True)
-    time = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return '<Meal %r>' % self.name
+    def __str__(self):
+        return f'{self.id} {self.name} {self.energy} {self.protein} {self.carbohydrate} {self.fat} {self.fiber} {self.external_id}'
 
 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String, nullable=False, unique=True)
+#     password = db.Column(db.String, nullable=False)
+#     # the following are per day (e.g., minimum grams of protein per day)
+#     energy_min = db.Column(db.Integer, nullable=True)
+#     energy_max = db.Column(db.Integer, nullable=True)
+#     protein_min = db.Column(db.Integer, nullable=True)
+#     protein_max = db.Column(db.Integer, nullable=True)
+#     carb_min = db.Column(db.Integer, nullable=True)
+#     carb_max = db.Column(db.Integer, nullable=True)
+#     fat_min = db.Column(db.Integer, nullable=True)
+#     fat_max = db.Column(db.Integer, nullable=True)
+
+#     def __repr__(self):
+#         return '<User %>' % self.username
+
+# # a meal is an individual food consumed by the user (meals do not have multiple foods)
+
+
+# class Meal(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     food_id = db.Column(db.Integer, db.ForeignKey('food.id'), nullable=False)
+#     name = db.Column(db.String, nullable=False)
+#     energy = db.Column(db.Integer, nullable=True)
+#     protein = db.Column(db.Integer, nullable=True)
+#     carb = db.Column(db.Integer, nullable=True)
+#     fat = db.Column(db.Integer, nullable=True)
+#     time = db.Column(db.DateTime, nullable=False)
+
+#     def __repr__(self):
+#         return '<Meal %r>' % self.name
